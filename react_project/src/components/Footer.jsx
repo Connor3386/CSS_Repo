e2ae89854b671ab8e91d91e7de12167c86ede2b7
Connor3386 <connorcox3386@gmail.com>
2026-02-27 09:09:08 -0800

    4.3

---
+++ react_project/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; 2026 Connor's high-quality snake oil. All Rights Reserved.</p>
+            <p>Email: connor'semail@email.com</p>
+            <p>Instagram: @connor'sinstagram</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ react_project/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+        <header>
+            <h1>Connor's high-quality snake oil</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Portfolio</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
